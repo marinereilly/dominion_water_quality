a91f68be76fa4121804c5738414e824e688fb267
Erin Reilly <erinmariereilly@gmail.com>
2018-02-09 10:04:06 -0500

    finished subsetting and doing hourly and daily averages for all variables

--- wq_wrangling.R
+++ wq_wrangling.R
@@ -32,6 +32,14 @@ salinity_day$date<- paste(salinity_day$Year,
                      salinity_day$Month, salinity_day$Day, sep = "-")
 salinity_day$date<-ymd(salinity_day$date)
 
+salinity_hour_hobo<-hobo %>% 
+  group_by(Year, Month, Day, hour) %>% 
+  summarise(daily_av=mean(salinity))
+salinity_hour_hobo$date_time<- paste(salinity_hour_hobo$Year,"-", 
+                                salinity_hour_hobo$Month, "-", salinity_hour_hobo$Day, " ",
+                                salinity_hour_hobo$hour, sep = "")
+salinity_hour_hobo$date_time<-ymd_h(salinity_hour_hobo$date_time)
+
 #####Water Level Averages#####
 wl_hour<-wq_2017 %>% 
   group_by(Station, Year, Month, Day, hour) %>% 
@@ -47,6 +55,22 @@ wl_day<-wq_2017 %>%
 wl_day$date<- paste(wl_day$Year, 
                     wl_day$Month, wl_day$Day, sep = "-")
 wl_day$date<-ymd(wl_day$date)
+
+#####Temp Averages#####
+temp_hour<-wq_2017 %>% 
+  group_by(Station, Year, Month, Day, hour) %>% 
+  summarise(hourly_av=mean(Temp))
+temp_hour$date_time<- paste(temp_hour$Year,"-", 
+                            temp_hour$Month, "-", temp_hour$Day, " ",
+                          temp_hour$hour, sep = "")
+temp_hour$date_time<-ymd_h(temp_hour$date_time)
+
+temp_day<-wq_2017 %>% 
+  group_by(Station, Year, Month, Day) %>% 
+  summarise(daily_av=mean(Temp))
+temp_day$date<- paste(temp_day$Year, 
+                      temp_day$Month, temp_day$Day, sep = "-")
+temp_day$date<-ymd(temp_day$date)
 
 #####DO Averages#####
 do_hour<-wq_2017 %>% 
@@ -65,3 +89,6 @@ do_day$date<- paste(do_day$Year,
                     do_day$Month, 
                     do_day$Day, sep = "-")
 do_day$date<-ymd(do_day$date)
+
+hypoxia<-wq_2017 %>% 
+  filter(., ODO_Conc<= 4)
