c6099fd23a63111685082bfddcf7519d2bd16e81
Erin Reilly <erinmariereilly@gmail.com>
2018-02-22 13:22:14 -0500

    Created the script for wrangling all year wq data!

---
+++ all_wq_wrangling.R
@@ -0,0 +1,37 @@
+#####Packages#####
+library(tidyverse)
+library(dadjoke)
+library(lubridate)
+
+#####Load data or data from Join all data script#####
+wq_all<-read.csv("Raw YSI Data/wq_2010_2017.csv")
+
+wq_all$DateTime<-ymd_hms(wq_all$DateTime)
+wq_all$Year<-as.factor(wq_all$Year)
+wq_all$Month<-as.factor(wq_all$Month)
+wq_all$YDay<-as.factor(wq_all$YDay)
+wq_all$Hour<-as.factor(wq_all$Hour)
+#Use this instead of ydays because it keeps it in date format for plotting
+#just make sure that the YEAR column exists as a factor/that you don't delete
+#the DateTime column
+wq_all$days<-as.Date(format(wq_all$DateTime,"%d-%m-2007"),format="%d-%m-%y")
+###Factor order for stations and Remove NA from year###
+wq_all$Station<-factor(wq_all$Station, levels = c("North", "Mid", "South"))
+
+#####Daily and Hourly Averages#####
+daily_av_2010_2017<-wq_all %>% 
+  group_by(Station, Year, days) %>% 
+  summarise_if(.predicate = function(x) is.numeric(x),
+               .funs = c(Mean="mean", Sd="sd"))
+hourly_av_2010_2017<-wq_all %>% 
+  group_by(Station, Year, days, Hour) %>% 
+  summarise_if(.predicate = function(x) is.numeric(x),
+               .funs = c(Mean="mean", Sd="sd"))
+
+
+
+
+#####Remove Crazy outliers#####
+#There are two Salinity averages above 500 which seems improbable but deleting
+#these values likely removes other values so I am going to attempt to remove
+#during the plotting process so it isn't permenant
