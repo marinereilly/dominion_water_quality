9e4011a19ec50e06179eb51dd82862d7772df891
Erin Reilly <erinmariereilly@gmail.com>
2018-02-27 11:22:33 -0500

    Make sweet plots

--- weather_plots.R
+++ weather_plots.R
@@ -3,6 +3,8 @@
 #####You should have already run the join data scripts
 
 #####start#####
+#####Helpful Stack overflow question!#####
+#https://stackoverflow.com/questions/26587940/ggplot2-different-legend-symbols-for-points-and-lines
 
 #####Load Packages#####
 library(tidyverse)
@@ -12,11 +14,51 @@ library(viridis)
 library(dadjoke)
 
 #####Load data if you haven't already#####
+#But honestly you should run through the weather wrangling and all wq wrangling scripts
 met_all<-read.csv("Raw YSI Data/met_all.csv")
 wq_all<-read.csv("Raw YSI Data/wq_2010_2017.csv")
+wq_2017<-wq_all %>% 
+  filter(Year=="2017")
 
 #####Formatting data if you haven't already#####
+#Again run through the wrangling scripts first. seriously. do this.
 met_all$Year<-as.factor(met_all$Year)
+met_daily_sum_2017$days<-ymd(met_daily_sum_2017$days)
+
+#####Palettes#####
+pal7<-c("North"="#E69F00", "Mid"="#F0E442", "South"="#009E73", "Hobo"="#56B4E9")
+
+pal8<-c("Precipitation"="darkorchid3", "Depth"="navy")
+pal9<-c("North"="#E69F00", "Mid"="#F0E442", "South"="#009E73","Depth"="navy", "Precipitation"="darkorchid3")
+
+#####Plots#####
+####Depth, Precipitation, Salinity####
+legend_ord<-c("North", "Mid", "South", "Depth", "Precipitation")
+
+z<-ggplot()+
+  geom_line(data=met_daily_sum_2017, aes(x=days, y=Rainfall_Sum, color= "Precipitation"))+
+  geom_point(data=salinity2, aes(x=days, y=Salinity_Mean, color=Station), size=0.6, 
+             na.rm = TRUE, alpha = 0.7)+
+  geom_line(data=depth, aes(x=days, y=av_depth, color="Depth"), linetype="twodash")+
+  theme_minimal()+
+  scale_color_manual(breaks= legend_ord, values = pal9)+
+  scale_x_date(date_labels = "%b", date_breaks = "month", 
+               limits = c(as.Date("2020-01-01"), as.Date("2020-12-31")))+
+  ggtitle("Depth, Precipitation and Salinity at Cove Point Marsh 2017")+
+  xlab("Date")+
+  ylab("Daily Cumulative Rainfall (in), Salinity (ppt), Depth (ft)")+
+  guides(color = guide_legend(title = "Stations",
+    override.aes = list(
+      size=c(2.5, 2.5, 2.5, 1, 1), 
+      shape = c(16, 16, 16, NA, NA), 
+      linetype = c("blank", "blank", "blank", "twodash", "solid"), 
+      alpha=1)))
+z
+
+
+
+
+
 
 #####Creating Monthly Daily and Hourly Averages/Accumulations####
 met_month<-met_all %>% 
