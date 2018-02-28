0ab303b4cfeebac86ca21d69e2a2c168fc2dc0c0
Erin Reilly <erinmariereilly@gmail.com>
2018-02-28 15:57:09 -0500

    Changed HOBO salinity plot...

--- wq_plots.R
+++ wq_plots.R
@@ -11,16 +11,17 @@ pal3<-c("North"="#FF4F00", "Mid"="#FF9500", "South"="#0C5DA5", "Hobo"="#00AC6B")
 pal4<-c("North"="#49E307", "Mid"="#06BA8D", "South"="#F30731", "Hobo"="#FF6408")
 pal5<-c("North"="#DE4500", "Mid"="#DE8200", "South"="#095090", "Hobo"="#00965D")
 pal6<-c("North"="#E69F00", "Mid"="#56B4E9", "South"="#009E73", "Hobo"="#F0E442")
-pal7<-c("North"="#E69F00", "Mid"="#F0E442", "South"="#009E73", "Hobo"="#56B4E9")
+pal7<-c("North"="#E69F00", "Mid"="#F0E442", "South"="#009E73", "Hobo"="#56B4E9", "Kayak"="#56B4E9")
 
 #####2017 Plots######
 ###Salinity###
 #Hourly average Salinity for all stations
 a<-ggplot()+
-  geom_point(data=salinity_hour_hobo, aes(x=date_time, y=hourly_av, color= Station),size =0.5)+
-  geom_point(data=salinity_hour, aes(x=date_time, y=hourly_av, color= Station), size=0.5)+
+  geom_point(data=hobo_daily_av_2010_2017, aes(x=days, y=salinity_Mean, color= station),shape=18 ,size =1)+
+  geom_point(data=daily_av_2017, aes(x=days, y=Salinity_Mean, color= Station), size=0.9)+
   theme_minimal()+
   scale_colour_manual(values =pal7)+
+  scale_x_date(date_labels =  "%b")+
   ggtitle("Average Hourly Salinity at Cove Point 2017")+
   ylab("Salinity (ppt)")+
   xlab("Date")+
@@ -31,8 +32,8 @@ ggsave(filename = "2017_hourly_sal.svg")
 
 #Daily average salinity for all stations
 b<-ggplot()+
-  geom_line(data=salinity_day_hobo2, aes(x=date, y=daily_av, color= Station), na.rm = FALSE, size=1)+
-  geom_line(data=salinity_day2, aes(x=date, y=daily_av, color= Station), na.rm = FALSE, size =1)+
+  geom_line(data=hobo_daily_av_2010_2017, aes(x=days, y=salinity_Mean, color= station), na.rm = FALSE, size=1)+
+  geom_line(data=daily_av_2017, aes(x=days, y=Salinity_Mean, color= Station), na.rm = FALSE, size =1)+
   theme_minimal()+
   scale_colour_manual(values =pal7)+
   ggtitle("Daily Average Salinity at Cove Point 2017")+
