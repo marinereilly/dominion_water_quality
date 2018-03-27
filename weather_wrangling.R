e703917acbf8b6608920aba0a038965afb586623
Erin Reilly <erinmariereilly@gmail.com>
2018-03-27 12:15:06 -0400

    added calculations for water year and two week cumulative rainfall
    deleted NA values
    
    seasonal wind
    
    monthly rainfall
    
    combined water year with aerial imagery

--- weather_wrangling.R
+++ weather_wrangling.R
@@ -17,6 +17,12 @@ met_all$days<-as.Date(format(met_all$DateTime,"%d-%m-2007"),format="%d-%m-%y")
 
 met_all$Year<-as.factor(met_all$Year)
 
+delete.na <- function(DF, n=0) {
+  DF[rowSums(is.na(DF)) <= n,]
+}
+
+met_all<-delete.na(met_all, 3)
+
 met_2017<-met_all %>% 
   filter(Year=="2017")
 met_2017$Year<-as.factor(met_2017$Year)
@@ -36,6 +42,12 @@ met_monthly_sum_2010_2017<-met_all %>%
   group_by(Year, Month) %>% 
   summarise_if(.predicate = function(x) is.numeric(x),
                .funs = c(Sum="sum"), na.rm=TRUE)
+av_monthly_rainfall<-met_monthly_sum_2010_2017 %>% 
+  select(Year, Month, Rainfall_Sum) %>% 
+  group_by(Month) %>% 
+  summarise(av_rainfall=mean(Rainfall_Sum), sd_rainfall=sd(Rainfall_Sum))
+av_monthly_rainfall$rf2017<-met_monthly_sum_2017$Rainfall_Sum  
+av_monthly_rainfall$difference<-av_monthly_rainfall$av_rainfall-av_monthly_rainfall$rf2017
 
 met_daily_av_2010_2017$Year<-as.factor(met_daily_av_2010_2017$Year)
 
@@ -135,6 +147,29 @@ ws_zero<-wind_freq_strength_2017 %>%
 ws_zero$grouped_wind<-360
 ws_2017<-wind_freq_strength_2017 %>% 
   bind_rows(., ws_zero)
+wind_freq<-na.omit(wind_freq)
+
+met_all$Season<-if_else(met_all$Month==1|met_all$Month==2|met_all$Month==12, "Winter", 
+                        if_else(met_all$Month==3|met_all$Month==4|met_all$Month==5, "Spring",
+                                if_else(met_all$Month==6|met_all$Month==7|met_all$Month==8,"Summer", "Autumn")
+))
+met_all$Season<-as.factor(met_all$Season)
+
+
+wind_freq_seasonal<-met_all%>% 
+  group_by(Season,Year) %>% 
+  count(., grouped_wind)
+wind_zero_seasonal<-wind_freq_seasonal %>% 
+  filter(grouped_wind==0)
+wind_zero_seasonal$grouped_wind<-360
+wind_freq_seasonal<-wind_freq_seasonal %>% 
+  bind_rows(., wind_zero_seasonal)
+wfs2017<-wind_freq_seasonal %>% 
+  filter(Year==2017)
+
+
+
+
 
 ######Calc for table#####
 met_calc<-met_all %>% 
@@ -158,3 +193,37 @@ salinity<-hourly_av_2010_2017 %>%
   filter(Year=="2017")
 salinity2<-daily_av_2010_2017 %>% 
   filter(Year=="2017")
+
+
+#####Water Year Calculations and Cumulative Plot#####
+View(met_daily_sum_2010_2017)
+day_rain<-met_daily_sum_2010_2017 %>% 
+  select(Year, days, Rainfall_Sum)
+day_rain$month<-month(day_rain$days)
+day_rain$day<-day(day_rain$days)
+day_rain$date<-paste0(day_rain$month, "-", day_rain$day, "-", day_rain$Year)
+day_rain$date<-mdy(day_rain$date)
+day_rain<-arrange(day_rain, date)
+day_rain<-select(day_rain, date, month, day, Year, Rainfall_Sum, days)
+day_rain$water_year<-if_else(day_rain$month<10, day_rain$Year, day_rain$Year+1)
+day_rain<-day_rain %>% 
+  group_by(water_year) %>% 
+  arrange(date) %>% 
+  mutate(wy_cum_rainfall = cumsum(Rainfall_Sum))
+day_rain$days2<-if_else(day_rain$month<10, 
+                        paste0(day_rain$month, "-", day_rain$day, "-", 2000), paste0(day_rain$month, "-", day_rain$day, "-", 1999))
+day_rain$days2<-mdy(day_rain$days2)
+two_week_sum <- function(x,n=14){filter(x,rep(1,n), sides=1)}
+day_rain$two_week_cum_rainfall<-rollsumr(day_rain$Rainfall_Sum, k=14, fill=NA)
+
+#####Aerial FLight Rain Calcs#####
+library(readxl)
+ai_flight <- read_excel("H:/0_HarrisLab/1_CURRENT PROJECT FOLDERS/Dominion/DATA/aerial_imagery_metadata.xlsx")
+View(ai_flight)
+ai_flight<-select(ai_flight, GBA_ref=`GBA Reference Number`,
+                  date = `Date Flown`)
+ai_flight$date<-mdy(ai_flight$date)
+ai_flight<-ai_flight %>% 
+  left_join(., day_rain, by="date")
+ai_flight<- ai_flight %>% 
+  select(GBA_ref, date, month, day, Year, water_year, Rainfall_Sum, wy_cum_rainfall, two_week_cum_rainfall)
