ad7af6fc2ee5f2c209feda69332ffed6d7b4f06b
Erin Reilly <erinmariereilly@gmail.com>
2019-02-04 12:02:51 -0500

    Created Custom plot variables

---
+++ palettes_and_plot_things.R
@@ -0,0 +1,11 @@
+#####plot palettes#####
+
+year_color_highlight<-c("2018"="red3", "2017"="grey75", "2016"="grey75", "2015"="grey75", "2014"="grey75", "2013"="grey75", "2012"="grey75", "2011"="grey75")
+year_size_highlight<-c("2018"=1.5, "2017"=1, "2016"=1, "2015"=1, "2014"=1, "2013"=1, "2012"=1, "2011"=1)
+
+pal7<-c("North"="#E69F00", "Mid"="#F0E442", "South"="#009E73", "Hobo"="#56B4E9")
+pal8<-c("Precipitation"="darkorchid3", "Depth"="navy")
+pal9<-c("North"="#E69F00", "Mid"="#F0E442", "South"="#009E73","Depth"="navy", "Precipitation"="darkorchid3")
+
+ypal2018 <- c("2011"="#fde0dd", "2012"="#fcc5c0", "2013"="#fa9fb5", "2014"="#f768a1", 
+           "2015"="#dd3497", "2016"="#ae017e", "2017"="#7a0177", "2018"="#49006a", "2010"="grey86")
